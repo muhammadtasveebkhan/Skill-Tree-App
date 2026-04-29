3a37ba692c15e2ef61d2478da76b3110817b8bf9
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-29 02:04:34 +0000

    Apr 28, 2026, 10:04 PM

--- src/main.py
+++ src/main.py
@@ -12,4 +12,7 @@ DATA_PATH = os.path.join(BASE_DIR, "data", "skills.csv")
 # Create the folder and file if they are missing so the app doesn't crash
 if not os.path.exists(os.path.dirname(DATA_PATH)):
     os.makedirs(os.path.dirname(DATA_PATH))
-    
+
+if not os.path.exists(DATA_PATH):
+    # Create the CSV with headers
+    df_start     
