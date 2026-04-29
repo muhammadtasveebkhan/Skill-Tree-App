cacfa44a121949fed816701e5a740d4461e7b28d
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-29 02:52:31 +0000

    Apr 28, 2026, 10:52 PM

--- src/main.py
+++ src/main.py
@@ -45,4 +45,4 @@ with st.form("entry_form", clear_on_submit=True):
 if submit_button:
     new_data = pd.DataFrame([[user_date, user_skill, user_hours, user_notes]],
                             columns=['Date', 'Skill', 'Hours', 'Notes'])
-                
+    # 'mode=a' means             
