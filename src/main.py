77389d9df650cec9a722b65cd550707f2a77616a
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-29 01:47:41 +0000

    Apr 28, 2026, 9:47 PM

--- src/main.py
+++ src/main.py
@@ -7,4 +7,5 @@ from datetime import datetime
 # --- STEP 1: SETUP FILE PATHS ---
 # This part makes sure the app finds the 'data' folder correctly
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "data", "skills.csv")
 
