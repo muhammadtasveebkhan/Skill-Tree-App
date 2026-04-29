11ef3e1d78e682d58dddf69e2fb3bba83f4def6b
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-29 02:29:02 +0000

    Apr 28, 2026, 10:29 PM

--- src/main.py
+++ src/main.py
@@ -33,4 +33,4 @@ with st.form("entry_form", clear_on_submit=True):
 
     with left:
         user_skill = st.selectbox("Select a Skill:", ["Python", "Data Science", "SQL", "Streamlit"])
-        
+        user_hours = st.number_input
