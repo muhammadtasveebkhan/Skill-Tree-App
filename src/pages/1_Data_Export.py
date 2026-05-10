8e971618a5bbd989333208d7832f643d8e63db3a
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-05-10 00:08:40 +0000

    May 9, 2026, 8:08 PM

--- src/pages/1_Data_Export.py
+++ src/pages/1_Data_Export.py
@@ -5,5 +5,9 @@ import os
 # Pathing
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FILE_PATH = os.path.join(BASE_DIR, "data", "skills.csv")
+
 st.title("Export Your Data")
 st.write("Download your skill-tree data for your resume or portfolio.")
+
+
+
