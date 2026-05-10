5021b79a105eb65ba625a8d33fdd544bf4190396
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-05-10 00:48:14 +0000

    May 9, 2026, 8:48 PM

--- src/pages/2_Resume_Builder.py
+++ src/pages/2_Resume_Builder.py
@@ -6,3 +6,6 @@ import os
 # Because we are inside the 'pages' folder, we have to go UP one level to find 'data'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FILE_PATH = os.path.join(BASE_DIR, "data", "skills.csv")
+
+# --- UI LAYOUT ---
+st.title
