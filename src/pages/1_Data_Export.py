3adaa693d38e27a5a964dc27a68ab5dee69adf1c
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-05-10 00:03:01 +0000

    May 9, 2026, 8:03 PM

--- src/pages/1_Data_Export.py
+++ src/pages/1_Data_Export.py
@@ -3,4 +3,4 @@ import pandas as pd
 import os
 
 # Pathing
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
